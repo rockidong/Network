250f6a5137ad375c5c5ec19a151d0906ca06bf0f
Robin <rockidong@me.com>
2016-04-27 04:11:26 +0900

    HttpHost 클래스의 인스턴스를 사용하여 HTTP 응답 메시지 수신 HttpHost 에서 HttpGet 으로 변경해서 실행

--- Network/src/ClientExecuteDirect.java
+++ Network/src/ClientExecuteDirect.java
@@ -1,8 +1,11 @@
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 /**
  * Created by Robin on 2016. 4. 27..
@@ -11,12 +14,35 @@ public class ClientExecuteDirect {
     public static void main(String[] args) throws Exception{
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
-        HttpHost target = new HttpHost("www.apache.org",80,"http");
-        HttpGet req  = new HttpGet("/");
+        try {
+//            HttpHost target = new HttpHost("www.apache.org",80,"http");
+            HttpGet req  = new HttpGet("http://www.apache.org/");
 
-        System.out.println("Executing request ot " + target);
+            System.out.println("Executing request ot " + req.getURI());
 
-        HttpResponse response = httpclient.execute(target,req);
+            HttpResponse response = httpclient.execute(req);
 
+            HttpEntity entity = response.getEntity();
+
+            System.out.println("------------------------");
+            System.out.println(response.getStatusLine());
+
+            System.out.println("------------------------");
+
+            Header[] header =  response.getAllHeaders();
+            for(int i = 0 ; i < header.length ; i++){
+    //            System.out.println(header[i].getName() + " : " + header[i].getValue());
+                System.out.println(header[i]);
+            }
+            System.out.println("------------------------");
+
+            if(entity != null)
+            {
+                System.out.println(EntityUtils.toByteArray(entity));
+            }
+        } finally {
+            httpclient.close();
+
+        }
     }
 }
